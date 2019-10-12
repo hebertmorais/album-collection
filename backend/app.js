e28c4b053d2535e061136f65a614aa8808331f0a
Hebert Morais <hebertlucashllm@gmail.com>
2019-10-11 23:21:49 -0300

    Adicionado express

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,12 @@
-const http = require('http');
+var express = require('express');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+var app = express();
 
-server.listen(port, hostname, () => {
+require('./api/index.js')(app);
+
+app.listen(port, function () {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
