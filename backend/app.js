9ce8104df0527870d90b0e4175af7ff4ce5991eb
Hebert Morais <hebertlucashllm@gmail.com>
2019-10-12 15:22:52 -0300

    Adicionado body parser

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 require('./api/index.js')(app);
 
