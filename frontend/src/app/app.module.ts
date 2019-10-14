a223ba51462146896b7d9ff4e5e56fccd132c461
Hebert Morais <hebertlucashllm@gmail.com>
2019-10-14 15:47:25 -0300

    Adicionados componentes de album/detalhe e classe modelo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlbumListComponent } from './album-list/album-list.component';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumListComponent,
+    AlbumDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/models/album.model.ts
@@ -0,0 +1,8 @@
+export class Album {
+    id: number;
+    album_name: string;
+    artist_name: string;
+    genre: string;
+    release_date: Date;
+    artwork: string;
+}
